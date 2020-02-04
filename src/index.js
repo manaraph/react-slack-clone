81cd43cc979a8c06be562742e62e3442214d9e1b
m4 <manaraph225@gmail.com>
2020-02-04 11:50:50 +0100

    Origanize project and create home, login and register route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
