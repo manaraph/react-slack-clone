3552f42d1258bde3a68cb5814b9550c27ce79073
m4 <manaraph225@gmail.com>
2020-02-04 17:46:42 +0100

    Register user with email and password on firebase

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,55 +1,107 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react'
+import firebase from '../../firebase';
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon,
+} from 'semantic-ui-react';
 
 class Register extends React.Component {
   state = {
     username: '',
     email: '',
     password: '',
-    passwordConfirmation: ''
+    passwordConfirmation: '',
   };
 
   handleChange = event => {
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+      [event.target.name]: event.target.value,
+    });
+  };
 
   handleSumit = event => {
-   event.preventDefault();
-  }
-
+    event.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(createdUser => {
+        console.log(createdUser);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   render() {
-    const { 
-      username,
-      email,
-      password,
-      passwordConfirmation
-    } = this.state;
+    const { username, email, password, passwordConfirmation } = this.state;
 
     return (
-      <Grid textAlign="center" verticalAlign="middle" className="app">
-        <Grid.Column style={{maxWidth: 450}}>
-          <Header as="h2" icon color="orange" textAlign="center">
-            <Icon name="puzzle piece" color="orange" />
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='puzzle piece' color='orange' />
             Register for DevChat
           </Header>
-          <Form onSubmit={this.handleSumit} size="large">
+          <Form onSubmit={this.handleSumit} size='large'>
             <Segment stacked>
-              <Form.Input fluid name="username" value={username} icon="user" iconPosition="left" placeholder="Username" onChange={this.handleChange} type="text" />
-              <Form.Input fluid name="email" value={email} icon="mail" iconPosition="left" placeholder="Email Address" onChange={this.handleChange} type="email" />
-              <Form.Input fluid name="password" value={password} icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password" />
-              <Form.Input fluid name="passwordConfirmation" value={passwordConfirmation} icon="repeat" iconPosition="left" placeholder="Confirm Password" onChange={this.handleChange} type="password" />
+              <Form.Input
+                fluid
+                name='username'
+                value={username}
+                icon='user'
+                iconPosition='left'
+                placeholder='Username'
+                onChange={this.handleChange}
+                type='text'
+              />
+              <Form.Input
+                fluid
+                name='email'
+                value={email}
+                icon='mail'
+                iconPosition='left'
+                placeholder='Email Address'
+                onChange={this.handleChange}
+                type='email'
+              />
+              <Form.Input
+                fluid
+                name='password'
+                value={password}
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                onChange={this.handleChange}
+                type='password'
+              />
+              <Form.Input
+                fluid
+                name='passwordConfirmation'
+                value={passwordConfirmation}
+                icon='repeat'
+                iconPosition='left'
+                placeholder='Confirm Password'
+                onChange={this.handleChange}
+                type='password'
+              />
 
-              <Button color="orange" fluid size="large">Submit</Button>
+              <Button color='orange' fluid size='large'>
+                Submit
+              </Button>
             </Segment>
           </Form>
-          <Message>Already a user? <Link to="/login">Login</Link></Message>
+          <Message>
+            Already a user? <Link to='/login'>Login</Link>
+          </Message>
         </Grid.Column>
       </Grid>
-    )
+    );
   }
 }
 
