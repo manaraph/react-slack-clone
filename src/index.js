cdb2d1ee94014080b52590f890685e2c69ea22d3
m4 <manaraph225@gmail.com>
2020-02-04 16:06:03 +0100

    Register form

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () => (
   <Router>
