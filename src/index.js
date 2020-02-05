e5fbae1b33a7037e335fab131c75eeddf7cd76bb
m4 <manaraph225@gmail.com>
2020-02-05 18:11:06 +0100

    Setup redux, add and configure reducers and actiosn

--- src/index.js
+++ src/index.js
@@ -6,17 +6,26 @@ import {
   Route,
   withRouter,
 } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import firebase from './firebase';
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import rootReducer from './reducers'
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        // console.log(user);
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
@@ -32,10 +41,12 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
-ReactDOM.render( 
-  <Router> 
-    <RootWithAuth /> 
-  </Router>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router> 
+      <RootWithAuth /> 
+    </Router>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
