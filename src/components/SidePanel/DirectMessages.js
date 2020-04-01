faa7f55bf871782979499dea74f6377d2632f62d
manaraph <manaraph225@gmail.com>
2020-04-01 21:36:00 +0100

    Display active direct message channel

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { setCurrentChannel, setPrivateChannel } from "../../actions";
 class DirectMessages extends React.Component {
   state = {
+    activeChannel: '',
     user: this.props.currentUser,
     users: [],
     usersRef: firebase.database().ref('users'),
@@ -72,6 +73,7 @@ class DirectMessages extends React.Component {
     }
     this.props.setCurrentChannel(channelData);
     this.props.setPrivateChannel(true);
+    this.setActiveChannel(user.uid);
   }
 
   getChannelId = userId => {
@@ -79,8 +81,12 @@ class DirectMessages extends React.Component {
     return userId < currentUserId ? `${userId}/${currentUserId}` : `${currentUserId}/${userId}`;
   }
 
+  setActiveChannel = userId => {
+    this.setState({ activeChannel: userId });
+  } 
+
   render() {
-    const { users } = this.state;
+    const { users, activeChannel } = this.state;
 
     return (
       <Menu.Menu className='menu'>
@@ -93,6 +99,7 @@ class DirectMessages extends React.Component {
         {users.map(user => (
           <Menu.Item
             key={user.uid}
+            active={user.uid === activeChannel}
             onClick={() => this.changeChannel(user)}
             style={{ opacity: 0.7, fontStyle: 'italic'}}
           >
